a39e1f1a360e65bb88a73c6256ecb48f4013f2db
André Souza <andrestephano@gmail.com>
2017-06-18 21:53:29 -0300

    fixed previous commit

--- spec/models/profiles/staff_spec.rb
+++ spec/models/profiles/staff_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe Profiles::Staff, type: :model do
     expect(profile.valid?).to eq(true)
   end
 
-  it "give manage user permission" do
-    right1 = Profiles::Staff.new(permissions: "can_manage_user")
-    right2 = Profiles::Staff.new(permissions: "can_do_other_things can_manage_user")
+  it "should know if staff can manage other users" do
+    right1 = Profiles::Staff.new(permissions: "can_manage_users")
+    right2 = Profiles::Staff.new(permissions: "can_do_other_things can_manage_users")
     wrong1 = Profiles::Staff.new(permissions: "do_nothing")
     expect(right1.can_manage_users?).to eq(true)
     expect(right2.can_manage_users?).to eq(true)
